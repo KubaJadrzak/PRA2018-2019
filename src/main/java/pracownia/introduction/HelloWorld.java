0a8064a3e2f1925a55ebc36731d11a391f18b807
KubaJadrzak <kuba.jadrzak@gmail.com>
2020-11-19 00:51:21 +0100

    IntroductionClassesWithLogger

--- src/main/java/pracownia/introduction/HelloWorld.java
+++ src/main/java/pracownia/introduction/HelloWorld.java
@@ -1,8 +1,12 @@
 package pracownia.introduction;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
 
+    static final Logger log = Logger.getLogger(HelloWorld.class);
     public static void main(String [ ] args) {
         System.out.println("Hello World");
+        log.info("message");
     }
 }
